c1dc2458962415c316d639c897bd17270f1597ad
vidihawk <fabian@demortier.net>
2022-12-28 15:28:42 +0000

    completed the geocoding loop

--- json_to_geojson.py
+++ json_to_geojson.py
@@ -4,8 +4,8 @@ import simplejson as json  # pip3 install simplejson
 
 # run the script in terminal by doing `python3 json_to_geojson.py input_file.json output_file.json``
 # script, in_file, out_file = argv
-in_file = 'data/json_to_geojson/in/test.json'
-out_file = 'data/json_to_geojson/out/test.geojson'
+in_file = 'data/json_to_geojson/in/global.json'
+out_file = 'data/json_to_geojson/out/global_geojson.json'
 
 data = json.load(open(in_file))
 
--- reverse_geocoding.py
+++ reverse_geocoding.py
@@ -2,6 +2,8 @@ from urllib.request import urlopen
 import json
 from dotenv import load_dotenv # pip3 install python-dotenv
 import os
+import gc
+import time
 
 load_dotenv()
 token = os.environ.get("MAPBOX_TOKEN")
@@ -13,41 +15,77 @@ token = os.environ.get("MAPBOX_TOKEN")
 
 lng = -6.231504071377264
 lat = 53.34927043775156
-in_file = 'data/reverse_geocoding/in/test.geojson'
-destination = "data/reverse_geocoding/out/addresses_added.geojson"
-max_api_calls = 10
+in_file = 'data/reverse_geocoding/global_geojson.json'
+destination = "data/reverse_geocoding/global_geojson.json"
+max_api_calls = 100000
 
 
-data_json = json.load(open(in_file))
-
-# get all coordinates from origin geojson file:
-api_calls = 1
-for i in data_json["features"]:
-    # get coordinates from source file:
-    coordinates = i["geometry"]["coordinates"]
-    lng = coordinates[0]
-    lat = coordinates[1]
-    # fetch reverse geoding data based on coordinates
-    url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{lng},{lat}.json?types=address&access_token={token}"
-    response = urlopen(url)
-    response_json = json.loads(response.read())
-    print(response_json)
-    if response_json['features']:
-        if response_json['features'][0].get('place_name') is not None:
-            address = response_json['features'][0]['place_name']
-            i["properties"]["address"] = address
+def load_and_dump(index, api_calls):
+    data_json = json.load(open(in_file))
+    existing_addresses = 0
+    for x in range(10):
+        feat_json = data_json["features"][index]
+        if "address" in feat_json["properties"]:
+            existing_addresses +=1
+            index += 1
+            continue
         else:
-            i["properties"]["address"] = "unknown"
+            if "coordinates" in feat_json["geometry"]:
+                coordinates = feat_json["geometry"]["coordinates"]
+                lng = coordinates[0]
+                lat = coordinates[1]
+                # fetch reverse geoding data based on coordinates
+                url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{lng},{lat}.json?proximity={lng},{lat}&types=address&access_token={token}"
+                '''the Geocoding API is limited to 600 requests per min.
+                At 0.05 we are at 180 r/min and still experience throttling
+                At 0.1 we are at 140 r/min and still experience throttling
+                At 0.2 we are at 120 r/min
+                '''
+                time.sleep(0.2) 
+                response = urlopen(url)
+                response_json = json.loads(response.read())
+                # print(response_json)
+                if response_json['features']:
+                    if response_json['features'][0].get('place_name') is not None:
+                        address = response_json['features'][0]['place_name']
+                        data_json["features"][index]["properties"]["address"] = address
+                        # print(address)
+                else:
+                    data_json["features"][index]["properties"]["address"] = "unknown"
+                    # if "country_long" in i["properties"]:
+                    #     print("no address found in ", i["properties"]["country_long"])
+                api_calls += 1
+                if api_calls >= max_api_calls:
+                    break
+            else:
+                print("no coordinates")
+            index += 1
+            del response, response_json
+    # dump data to json file
+    if existing_addresses != 10:
+        out_file = open(destination, "w")
+        json.dump(data_json, out_file, indent = 4)
+        out_file.close()
+        del out_file
     else:
-        pass
-    print(address)
-    print(api_calls)
-    api_calls += 1
-    if api_calls >= max_api_calls:
-        break
+        print("addresses already exists")
+    del data_json
+    gc.collect()
+    return index, api_calls
 
-# dump data to json file
-out_file = open(destination, "w")
-json.dump(data_json, out_file, indent = 6)
-out_file.close()
+index = 3250
+api_calls = 0
+
+for i in range(max_api_calls):
+    index, api_calls = load_and_dump(index, api_calls)
+    print("index ", index)
+    print("api call ", api_calls)
+    # if index % 1000 == 0:
+    #     print(f"index: {index}, api calls: {api_calls}")
+    #     input("Press any key to continue...")
+        
+
+
+
+
 
