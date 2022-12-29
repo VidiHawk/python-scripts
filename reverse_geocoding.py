2e4ca65a65aca4c31f1cfb31ad111a1426a1f1a9
vidihawk <fabian@demortier.net>
2022-12-29 11:46:27 +0000

    update

--- reverse_geocoding.py
+++ reverse_geocoding.py
@@ -5,6 +5,7 @@ import os
 import gc
 import time
 
+
 load_dotenv()
 token = os.environ.get("MAPBOX_TOKEN")
 
@@ -34,15 +35,22 @@ def load_and_dump(index, api_calls):
                 coordinates = feat_json["geometry"]["coordinates"]
                 lng = coordinates[0]
                 lat = coordinates[1]
+                api_calls += 1
                 # fetch reverse geoding data based on coordinates
                 url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{lng},{lat}.json?proximity={lng},{lat}&types=address&access_token={token}"
                 '''the Geocoding API is limited to 600 requests per min.
                 At 0.05 we are at 180 r/min and still experience throttling
                 At 0.1 we are at 140 r/min and still experience throttling
-                At 0.2 we are at 120 r/min
+                At 0.2 we are at 120 r/min and still experience throttling
+                At 0.5 we are at 75 r/min 
                 '''
-                time.sleep(0.2) 
-                response = urlopen(url)
+                time.sleep(0.5) 
+                try:
+                    response = urlopen(url, timeout=5)
+                except Exception:
+                    print("TIMEOUT! sleeping for a minute...")
+                    time.sleep(60)
+                    continue 
                 response_json = json.loads(response.read())
                 # print(response_json)
                 if response_json['features']:
@@ -54,7 +62,6 @@ def load_and_dump(index, api_calls):
                     data_json["features"][index]["properties"]["address"] = "unknown"
                     # if "country_long" in i["properties"]:
                     #     print("no address found in ", i["properties"]["country_long"])
-                api_calls += 1
                 if api_calls >= max_api_calls:
                     break
             else:
@@ -73,7 +80,7 @@ def load_and_dump(index, api_calls):
     gc.collect()
     return index, api_calls
 
-index = 3250
+index = 6450
 api_calls = 0
 
 for i in range(max_api_calls):
